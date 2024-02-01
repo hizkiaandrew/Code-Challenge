32146c9f6a829b1348d606e89a1947313ff20848
Hizkia Andrew <hizkiaandrew@gmail.com>
2024-02-01 21:43:17 +0700

    Day 1 - Even or Odd

---
+++ 1-EvenOrOdd.js
@@ -0,0 +1,9 @@
+//Hizkia Code Challenge - Day 1
+// Even or Odd
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+
+function evenOrOdd(number) {
+  return number % 2 == 0 ? "Even" : "Odd";
+}
+
+console.log(evenOrOdd(0));
